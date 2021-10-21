0798db77c42e648566a9cf35688c852cc47db658
MarcoVinciguerra <mvincig11@gmail.com>
2021-10-21 21:55:21 +0200

    update exe 1

---
+++ Esercitazione1/src/Pippo.java
@@ -0,0 +1,24 @@
+public class Pippo {
+    
+    public Pippo(){
+        //#ifdef Directed
+        System.out.println("Directed");
+        //#endif
+
+        //#ifdef Undirected
+        System.out.println("Undirected");
+        //#endif
+
+        //#ifdef Number
+        System.out.println("Number");
+        //#endif
+
+        //#ifdef Cycle
+        System.out.println("Cycle");
+        //#endif
+    }
+
+    public static void main(string[] args) {
+        Pippo p = new Pippo();
+    }
+}
